e605daabcec315461c9803696b4fef73c290c72f
gerlaju <juergen_thomas.gerlach@mercedes-benz.com>
2023-03-11 21:44:51 +0100

    add json parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+pub mod parser;
+
 fn main() {
     println!("Hello, world!");
+    // parser::typed_example();
+
+    let result = parser::typed_example();
+
+    match result {
+        Ok(()) => (),
+        Err(error) => print!("{}", error),
+    }
 }
